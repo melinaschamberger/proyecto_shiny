b2a95fc5b213509c07b9e095aa967a7d48ae7748
melinaschamberger <melina.schamberger@gmail.com>
2022-01-10 22:15:26 -0300

    Actualizo

--- porc_ocupacion_mujeres_sector_actividad.R
+++ porc_ocupacion_mujeres_sector_actividad.R
@@ -4,7 +4,10 @@
 #Librerías
 library(readr)
 library(tidyverse)
+library(lubridate)
+library(ggthemes)
 
+## 1. Cargo datos
 
 #Porcentaje de mujeres en el sector privado, por sector de actividad
 dos_digitos <- read_csv("https://raw.githubusercontent.com/melinaschamberger/proyecto_shiny/main/Datos/Mujeres_sector_privado/share_mujer_privado_mensual_por_clae2_06122021.csv")
@@ -28,10 +31,124 @@ empresas_letras <- read_csv("https://raw.githubusercontent.com/melinaschamberger
 empresas_total <- read_csv("https://raw.githubusercontent.com/melinaschamberger/proyecto_shiny/main/Datos/Mujeres_sector_privado/share_mujer_total_empresas_total_06122021.csv")
 
 #Diccionario de clases: porcentaje de ocupación femenina por sector privado y sector de actividad
-datos_sector_privado <- read_csv("https://datos.produccion.gob.ar/dataset/162dc5a6-7a95-4f19-9d85-063c9be23208/resource/145dd127-630e-40a5-9a31-239e34a01d36/download/clae_diccionario.csv")
-datos_sector_privado <- left_join(datos_sector_privado, dos_digitos)
-datos_sector_privado <- left_join(datos_sector_privado, tres_digitos)
-datos_sector_privado <- left_join(datos_sector_privado, seis_digitos)
-datos_sector_privado <- left_join(datos_sector_privado, letras)
-datos_sector_privado <- left_join(datos_sector_privado, privado_total)
+datos <- read_csv("https://datos.produccion.gob.ar/dataset/162dc5a6-7a95-4f19-9d85-063c9be23208/resource/145dd127-630e-40a5-9a31-239e34a01d36/download/clae_diccionario.csv")
+
+## 2. Analizo participación femenina en sector privado (clasificacion de dos digitos)
+colnames(datos)
+sp_dos <- datos %>%  select(5:6)
+sp_dos <- unique(sp_dos)
+sp_dos <- left_join(dos_digitos, sp_dos)
+sp_dos <- sp_dos %>% mutate(anio = year(fecha))
+#promedio anual en clasificación de dos digitos
+sp_dos <- sp_dos %>% 
+  group_by(anio, clae2_desc, clae2) %>% 
+  summarise(porc_medio = round(mean(por_mu_sector_privado_clae2)*100,2))
+                  
+## 3. Analizo participación femenina en sector privado (clasificacion de tres digitos)
+colnames(datos)
+sp_tres.a<- datos %>%  select(3:6)
+sp_tres <- unique(sp_tres.a)
+sp_tres <- left_join(tres_digitos, sp_tres)
+sp_tres <- sp_tres %>% mutate(anio = year(fecha))
+#promedio anual en clasificación de tres digitos
+sp_tres <- sp_tres %>% 
+  group_by(anio, clae3_desc, clae3) %>% 
+  summarise(porc_medio = round(mean(por_mu_sector_privado_clae3)*100,2))
+sp_tres <- left_join(sp_tres.a, sp_tres)
+sp_tres <- unique(sp_tres)
+
+## 4. Analizo participación femenina en sector privado (clasificacion de seis digitos)
+colnames(datos)
+sp_seis.a<- datos %>%  select(1:6)
+sp_seis <- unique(sp_seis.a)
+sp_seis <- left_join(seis_digitos, sp_seis)
+sp_seis <- sp_seis %>% mutate(anio = year(fecha))
+#promedio anual en clasificación de tres digitos
+sp_seis <- sp_seis %>% 
+  group_by(anio, clae6_desc, clae6) %>% 
+  summarise(porc_medio = round(mean(por_mu_sector_privado_clae6)*100,2))
+sp_seis <- left_join(sp_seis.a, sp_seis)
+sp_seis <- unique(sp_seis)
+
+#-------------------------------------------------------------------------------
+## 5. Analizo en un gráficos los datos del 2
+
+#ejemplo: año 2017
+sp_dos_A <- sp_dos %>%  filter(anio == 2017) %>% arrange(-porc_medio) 
+sp_dos_A <- sp_dos_A[1:20,]  
+
+sp_dos_B <- sp_dos %>%  filter(anio == 2017) %>% arrange(porc_medio) 
+sp_dos_B <- sp_dos_B[1:20,]  
+
+#Defino la fuente
+windowsFonts(A = windowsFont("Roboto"))
+
+#Gráfico de mejores del 2017
+graf_dos <- ggplot(sp_dos_A, aes(x=reorder(clae2_desc, porc_medio), 
+                            y=porc_medio, 
+                            fill = porc_medio)) +  
+  geom_bar(stat="identity", 
+           width = 0.4) + 
+  coord_flip() + 
+  geom_text(aes(label = porc_medio), 
+            colour = "black", 
+            fontface = "italic", 
+            family = "A",
+            size = 2.5, 
+            hjust = -0.10) + 
+  xlab("Sector") + 
+  ylab(" ") + 
+  labs(
+    title = "Mujeres ocupadas por sector de actividad (%)",
+    caption = "Fuente: Elaboración propia en base a datos provistos por el Ministerio de Desarrollo Productivo (2021).",
+    subtitle = "20 sectores con mayor participación femenina."
+  )  +
+  theme_minimal() + 
+  theme(legend.position = "null", 
+        text=element_text(size=12, 
+                          family = "A", 
+                          colour = "#50535C"),
+        plot.caption = element_text(hjust = 0.85)) + 
+  scale_fill_gradientn(colours = c("#FAAE7B", "#CC8B79","#9F6976","#714674","#432371", "#3d2067"),
+                       values = scales::rescale(c(45,50,60,70,80, 88))) +
+  scale_y_continuous(limits = c(0, 90),
+                     breaks = seq(0, 90, by = 10), 
+                     labels = sprintf("%.f%%", seq(0,90, by = 10)))
+
+ggsave("graf_dos.png", width = 40, height = 20, units = "cm")
+
+#Gráfico de peores del 2017
+graf_tres <- ggplot(sp_dos_B, aes(x=reorder(clae2_desc, -porc_medio), 
+                                 y=porc_medio, 
+                                 fill = porc_medio)) +  
+  geom_bar(stat="identity", 
+           width = 0.4) + 
+  coord_flip() + 
+  geom_text(aes(label = porc_medio), 
+            colour = "black", 
+            fontface = "italic", 
+            family = "A",
+            size = 2.5, 
+            hjust = -0.10) + 
+  xlab("Sector") + 
+  ylab(" ") + 
+  labs(
+    title = "Mujeres ocupadas por sector de actividad (%)",
+    subtitle = "20 sectores con menor participación femenina.",
+    caption = "Fuente: Elaboración propia en base a datos provistos por el Ministerio de Desarrollo Productivo (2021)."
+  )  +
+  theme_minimal() + 
+  theme(legend.position = "null", 
+        text=element_text(size=12, 
+                          family = "A", 
+                          colour = "#50535C"),
+        plot.caption = element_text(hjust = 0.85)) + 
+  scale_fill_gradientn(colours = c( "#3D2067", "#714674","#B67A78","#D8947A", "#FAAE7B","#FAB587"),
+                       values = scales::rescale(c(11,8,5,2,0))) +
+  scale_y_continuous(limits = c(0, 12),
+                     breaks = seq(0, 12, by = 2), 
+                     labels = sprintf("%.f%%", seq(0,12, by = 2)))
+ggsave("graf_tres.png", width = 40, height = 20, units = "cm")
+
+
 
