55d7630068fa0d7dcc5b60e62aecfe6b1ae97c8c
Natasha Siderman <97059036+natashasiderman@users.noreply.github.com>
2022-01-11 17:05:35 -0300

    Add files via upload

--- Shiny/server.R
+++ Shiny/server.R
@@ -5,6 +5,7 @@ library(rsconnect)
 
 
 total_grado_pregrado <- read.csv("total_grado_pregrado.csv")
+facultades_uba <- read.csv("facultades_uba.csv")
 
 server <- function(input, output) {
   
@@ -16,6 +17,27 @@ server <- function(input, output) {
     color_genero <- c("Femenino" = "darkviolet", "Masculino" = "grey")
     
       ggplot(data=sub_grado_pregrado) +
+      geom_bar(mapping = aes(x = Género, y = Porcentaje, fill=Género), 
+               stat = "identity")+
+      scale_fill_manual(values = color_genero) +
+      labs(x = "Género", 
+           y = "Porcentaje")+
+      theme_minimal() +
+      theme(legend.position = "none") +
+      geom_text(aes(x = Género, y = Porcentaje, label = Porcentaje), 
+                colour = "black",
+                position=position_dodge(width=0.9), 
+                vjust=-0.25)
+  })
+  
+  output$facultades_uba_bars <-renderPlot({
+    
+    sub_facultades_uba <- facultades_uba %>%
+      filter(Facultad == input$facultad)
+    
+    color_genero <- c("Femenino" = "darkviolet", "Masculino" = "grey")
+    
+    ggplot(data=sub_facultades_uba) +
       geom_bar(mapping = aes(x = Género, y = Porcentaje, fill=Género), 
                stat = "identity")+
       scale_fill_manual(values = color_genero) +
--- Shiny/ui.R
+++ Shiny/ui.R
@@ -4,6 +4,7 @@ library(shinythemes)
 library(rsconnect)
 
 total_grado_pregrado <- read.csv("total_grado_pregrado.csv")
+facultades_uba <- read.csv("facultades_uba.csv")
 
 ui <- fluidPage(theme = shinytheme("sandstone"),
                 title = "Trayectoria académica e inserción laboral femenina",
@@ -23,4 +24,15 @@ ui <- fluidPage(theme = shinytheme("sandstone"),
                                      mainPanel(plotOutput(outputId = "total_grado_pregrado_bars", height = 500)),
                                      tabPanel("Referencias",
                                               p(HTML("<b>Estudiantes, nuevas/os inscriptas/os y egresadas/os de títulos de pregrado y grado por género según sector de gestión. Año 2019.</b>")),																								
-                                              p(HTML("<b>Fuente: Anuario de Estadísticas Universitarias 2019 - Ministerio de Educación </b>"))))))
+                                              p(HTML("<b>Fuente: Anuario de Estadísticas Universitarias 2019 - Ministerio de Educación </b>")))),
+                            tabPanel("Facultades UBA", fluid = TRUE,
+                                     titlePanel("Participación femenina en facultades UBA"),
+                                     sidebarPanel(
+                                       selectInput(inputId = "facultad", 
+                                                   label = strong("Facultad"),
+                                                   choices = unique(facultades_uba$Facultad),
+                                                   selected = "Femenino")),
+                                     mainPanel(plotOutput(outputId = "facultades_uba_bars", height = 500)),
+                                     tabPanel("Referencias",
+                                              p(HTML("<b>Estudiantes de la Universidad de Buenos Aires según facultad y género. Año 2016.</b>")),																								
+                                              p(HTML("<b>Fuente: UBA - Información Estadística </b>"))))))
