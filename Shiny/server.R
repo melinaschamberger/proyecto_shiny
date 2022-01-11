e1d1947fc47963025a9f9f6e28b4ef1b3aaf9bed
Natasha Siderman <97059036+natashasiderman@users.noreply.github.com>
2022-01-11 15:15:37 -0300

    Add files via upload

---
+++ Shiny/server.R
@@ -0,0 +1,29 @@
+library(tidyverse)
+library(shiny)
+library(shinythemes)
+library(rsconnect)
+
+total_grado_pregrado <- read.csv("total_grado_pregrado.csv")
+
+server <- function(input, output) {
+  
+  output$total_grado_pregrado_bars <-renderPlot({
+    
+    sub_grado_pregrado <- total_grado_pregrado %>%
+      filter(Categoría == input$categoria & Gestión == input$gestion)
+    
+    ggplot(data=sub_grado_pregrado) +
+      geom_bar(mapping = aes(x = Género, y = Porcentaje, fill=Porcentaje), 
+               stat = "identity")+
+      labs(x = "Género", 
+           y = "Porcentaje")+
+      theme_minimal() +
+      theme(legend.position = "none") +
+      geom_text(aes(x = Género, y = Porcentaje, label = Porcentaje), 
+                colour = "black",
+                position=position_dodge(width=0.9), 
+                vjust=-0.25)
+  })
+  
+  }
+
---
+++ Shiny/ui.R
@@ -0,0 +1,26 @@
+library(tidyverse)
+library(shiny)
+library(shinythemes)
+library(rsconnect)
+
+total_grado_pregrado <- read.csv("total_grado_pregrado.csv")
+
+ui <- fluidPage(theme = shinytheme("yeti"),
+                title = "Trayectoria académica e inserción laboral femenina",
+                titlePanel(title = h1(strong("Trayectoria académica e inserción laboral femenina"), style="text-align: center")),
+                tabsetPanel(type = "tabs",
+                            tabPanel("Grado y pregrado", fluid = TRUE,
+                                     titlePanel("Participación femenina en carreras de grado y pregrado"),
+                                     sidebarPanel(
+                                       selectInput(inputId = "categoria", 
+                                                   label = strong("Categoría"),
+                                                   choices = unique(total_grado_pregrado$Categoría),
+                                                   selected = "Estudiantes"),
+                                       selectInput(inputId = "gestion", 
+                                                   label = strong("Gestión"),
+                                                   choices = unique(total_grado_pregrado$Gestión),
+                                                   selected = "Estatal")),
+                                     mainPanel(plotOutput(outputId = "total_grado_pregrado_bars", height = 500)),
+                                     tabPanel("Fuente",
+                                              p(HTML("")),
+                                              p(HTML("<b>Fuente: Anuario de Estadísticas Universitarias 2019 - Ministerio de Educación </b>"))))))
