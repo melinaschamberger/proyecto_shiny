a23b2826097455bcbd7e4ad5976e5c897e4e5458
Natasha Siderman <natashasiderman@gmail.com>
2022-01-12 13:55:51 -0300

    Agregué paso a paso

--- porc_mujeres_grado_y_pregrado.R
+++ porc_mujeres_grado_y_pregrado.R
@@ -1,14 +1,23 @@
+#Porcentaje de mujeres en carreras de grado y pregrado. Todas las facultades.
+#https://drive.google.com/file/d/1IiKj1ILMrR91J8-GCvVblA4UFjMZ3Eb9/view (datos transformados)
+
+#Librería
 library(tidyverse)
 
+#Cargo datos
 total <-  read.csv("total_r.csv", sep=";", encoding = "UTF-8")
 
+#Renombro variables
 total <- rename(total, Categoría = X.U.FEFF.Categoría)
 
+#Elimino variables que no voy a utilizar
 total$X <- NULL
 
+#Calculo porcentaje de participación femenina sobre el total
 total <- total %>%
   group_by(Categoría, Gestión) %>%
   mutate(Porcentaje = round (100* Cantidad/ sum(Cantidad),2))
 
+#Guardo archivo con los porcentajes
 write.csv(total,"C:/Users/Natasha/Desktop/Proyecto Mel/Proyecto Shiny/total_grado_pregrado.csv", row.names = F)
 
