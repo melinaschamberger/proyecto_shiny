0b0f809da21ddf84a8eb5b25d95410fecab81678
Natasha Siderman <natashasiderman@gmail.com>
2022-01-12 14:16:37 -0300

    Agregué paso a paso

--- Shiny/server.R
+++ Shiny/server.R
@@ -1,23 +1,26 @@
+#Cargo librerías
+
 library(tidyverse)
 library(shiny)
 library(shinythemes)
 library(rsconnect)
 
-
+#Cargo datos
 total_grado_pregrado <- read.csv("total_grado_pregrado.csv")
 facultades_uba <- read.csv("facultades_uba.csv")
 
+#Configuro reactividad
 server <- function(input, output) {
   
-  output$total_grado_pregrado_bars <-renderPlot({
+  output$total_grado_pregrado_bars <-renderPlot({ #Configuro el gráfico de barras de la primera pestaña
     
-    sub_grado_pregrado <- total_grado_pregrado %>%
-      filter(Categoría == input$categoria & Gestión == input$gestion)
+    sub_grado_pregrado <- total_grado_pregrado %>% #Configuro una sub-base de datos que 
+      filter(Categoría == input$categoria & Gestión == input$gestion) #filtre la principal según el input del usuario
     
-    color_genero <- c("Femenino" = "darkviolet", "Masculino" = "grey")
+    color_genero <- c("Femenino" = "darkviolet", "Masculino" = "grey") #Configuro los colores del gráfico
     
-      ggplot(data=sub_grado_pregrado) +
-      geom_bar(mapping = aes(x = Género, y = Porcentaje, fill=Género), 
+      ggplot(data=sub_grado_pregrado) + #Configuro el gráfico con los datos de la sub-base
+      geom_bar(mapping = aes(x = Género, y = Porcentaje, fill=Género), #Porcentaje de estudiantes según género
                stat = "identity")+
       scale_fill_manual(values = color_genero) +
       labs(x = "Género", 
@@ -30,15 +33,15 @@ server <- function(input, output) {
                 vjust=-0.25)
   })
   
-  output$facultades_uba_bars <-renderPlot({
+  output$facultades_uba_bars <-renderPlot({ #Configuro el gráfico de barras de la segunda pestaña
     
-    sub_facultades_uba <- facultades_uba %>%
-      filter(Facultad == input$facultad)
+    sub_facultades_uba <- facultades_uba %>% #Configuro una sub-base de datos que 
+      filter(Facultad == input$facultad) #filtre la principal según el input del usuario
     
-    color_genero <- c("Femenino" = "darkviolet", "Masculino" = "grey")
+    color_genero <- c("Femenino" = "darkviolet", "Masculino" = "grey") #Configuro los colores del gráfico
     
-    ggplot(data=sub_facultades_uba) +
-      geom_bar(mapping = aes(x = Género, y = Porcentaje, fill=Género), 
+    ggplot(data=sub_facultades_uba) + #Configuro el gráfico con los datos de la sub-base
+      geom_bar(mapping = aes(x = Género, y = Porcentaje, fill=Género), #Porcentaje de estudiantes según género
                stat = "identity")+
       scale_fill_manual(values = color_genero) +
       labs(x = "Género", 
--- Shiny/ui.R
+++ Shiny/ui.R
@@ -1,38 +1,43 @@
+#Cargo librerías
+
 library(tidyverse)
 library(shiny)
 library(shinythemes)
 library(rsconnect)
 
+#Cargo datos
 total_grado_pregrado <- read.csv("total_grado_pregrado.csv")
 facultades_uba <- read.csv("facultades_uba.csv")
 
-ui <- fluidPage(theme = shinytheme("sandstone"),
-                title = "Trayectoria académica e inserción laboral femenina",
-                titlePanel(title = h1(strong("Trayectoria académica e inserción laboral femenina"), style="text-align: center")),
-                tabsetPanel(type = "tabs",
-                            tabPanel("Grado y pregrado", fluid = TRUE,
-                                     titlePanel("Participación femenina en carreras de grado y pregrado"),
-                                     sidebarPanel(
-                                       selectInput(inputId = "categoria", 
-                                                   label = strong("Categoría"),
-                                                   choices = unique(total_grado_pregrado$Categoría),
-                                                   selected = "Estudiantes"),
-                                       selectInput(inputId = "gestion", 
-                                                   label = strong("Gestión"),
-                                                   choices = unique(total_grado_pregrado$Gestión),
-                                                   selected = "Estatal")),
-                                     mainPanel(plotOutput(outputId = "total_grado_pregrado_bars", height = 500)),
-                                     tabPanel("Referencias",
+#Configuro la interfaz del usuario
+ui <- fluidPage(theme = shinytheme("sandstone"), #elijo tema
+                title = "Trayectoria académica e inserción laboral femenina", #titulo la aplicación
+                titlePanel(title = h1(strong("Trayectoria académica e inserción laboral femenina"), 
+                                      style="text-align: center")), #titolo el panel
+                tabsetPanel(type = "tabs", #configuro la aplicación en pestañas
+                            tabPanel("Grado y pregrado", fluid = TRUE, #Configuro la primera pestaña
+                                     titlePanel("Participación femenina en carreras de grado y pregrado"), #titulo de la primera pestaña
+                                     sidebarPanel( #Configuro barra lateral desplegable
+                                       selectInput(inputId = "categoria", #Configuro el input categoría
+                                                   label = strong("Categoría"), #Etiqueta visible para el usuario
+                                                   choices = unique(total_grado_pregrado$Categoría), #Configuro opciones (valores únicos de variable "Categoría")
+                                                   selected = "Estudiantes"), #Opción por default
+                                       selectInput(inputId = "gestion", #Configuro el input gestión
+                                                   label = strong("Gestión"), #Etiqueta visible para el usuario
+                                                   choices = unique(total_grado_pregrado$Gestión), #Configuro opciones
+                                                   selected = "Estatal")), #Opción por default
+                                     mainPanel(plotOutput(outputId = "total_grado_pregrado_bars", height = 500)), #Configuro como output de la selección un gráfico de barras
+                                     tabPanel("Referencias", #Agrego referencias
                                               p(HTML("<b>Estudiantes, nuevas/os inscriptas/os y egresadas/os de títulos de pregrado y grado por género según sector de gestión. Año 2019.</b>")),																								
                                               p(HTML("<b>Fuente: Anuario de Estadísticas Universitarias 2019 - Ministerio de Educación </b>")))),
-                            tabPanel("Facultades UBA", fluid = TRUE,
-                                     titlePanel("Participación femenina en facultades UBA"),
-                                     sidebarPanel(
-                                       selectInput(inputId = "facultad", 
-                                                   label = strong("Facultad"),
-                                                   choices = unique(facultades_uba$Facultad),
-                                                   selected = "Femenino")),
-                                     mainPanel(plotOutput(outputId = "facultades_uba_bars", height = 500)),
-                                     tabPanel("Referencias",
+                            tabPanel("Facultades UBA", fluid = TRUE, #Configuro la segunda pestaña
+                                     titlePanel("Participación femenina en facultades UBA"), #titulo de la segunda pestaña
+                                     sidebarPanel( #Configuro barra lateral desplegable
+                                       selectInput(inputId = "facultad", #Configuro el input facultad
+                                                   label = strong("Facultad"), #Etiqueta visible para el usuario
+                                                   choices = unique(facultades_uba$Facultad), #Configuro opciones (valores únicos de variable "Facultad")
+                                                   selected = "Femenino")), #Opción por default
+                                     mainPanel(plotOutput(outputId = "facultades_uba_bars", height = 500)), #Configuro como output de la selección un gráfico de barras
+                                     tabPanel("Referencias", #Agrego referencias
                                               p(HTML("<b>Estudiantes de la Universidad de Buenos Aires según facultad y género. Año 2016.</b>")),																								
                                               p(HTML("<b>Fuente: UBA - Información Estadística </b>"))))))
